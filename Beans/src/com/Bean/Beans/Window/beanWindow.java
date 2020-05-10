9dc53dbf75114bfe836e97316a7bd4c714a87aac
Ahmed Mahamoud <ahmedhmahamoud99@gmail.com>
2020-05-10 02:40:14 -0500

    added menu bar with a menu item for the option to exit
    cleaned code and added some comments

--- Beans/src/com/Bean/Beans/Window/beanWindow.java
+++ Beans/src/com/Bean/Beans/Window/beanWindow.java
@@ -11,12 +11,22 @@
 package com.Bean.Beans.Window;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class beanWindow {
 	public JFrame frame;
-     public beanWindow(int width, int height, String title, mainGame game) {
+	public JMenuBar bar;
+	public JMenu menu;
+	public JMenuItem exit;
+	
+	
+	public beanWindow(int width, int height, String title, mainGame game) {
     	 // current window
     	  
     	 game.setPreferredSize(new Dimension(width, height));
@@ -31,7 +41,26 @@ public class beanWindow {
     	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	 frame.setResizable(false);
     	 frame.setLocationRelativeTo(null); // sets our current frame location 
-    	
+    	 // frame.setLayout(null);
+    	 // added menu bar
+    	 // added comments
+    	 bar = new JMenuBar();
+    	 bar.setBounds(0, 0, width, 25);
+    	 menu = new JMenu("Menu");
+    	 menu.setBounds(0, 0, 45, 24);
+    	 bar.add(menu);
+    	 
+    	 exit = new JMenuItem("Exit");
+    	 menu.add(exit);
+    	 exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}	 
+    	 });
+    	 
+ 
+       	 frame.add(bar);
+       	 frame.setJMenuBar(bar);
     	 frame.setVisible(true);
     	 game.Start();
      }
--- Beans/src/com/Bean/Beans/Window/mainGame.java
+++ Beans/src/com/Bean/Beans/Window/mainGame.java
@@ -3,29 +3,29 @@
  * Authors : Abdirahman Abdi
  *          :Dillon Zieglemann
  *          :Bruce Nyakundi
- *          :Ahmed Mohamoud
- * Course : OOP programming
+ *          :Ahmed Mahamoud
+ * Course : OOP programming / CSCI 1082-90
  * Instructor: Zakaria Baani
  * Due date: 05/10/2020
  */
 
-//pseudocode
-
-//1.extend canvas and implement runnable(thread)
-//2.create our window frame (has width,height and game)
-//3.intiialize our game 
-//4.create run,tick and render methods
-//5.create init method for intializing stuff
-//6.ability to loop thru and change frames
-//7.use buffer strategy
-//8.Use enums for ID AND GAME STATE
-//9.use a handler class that uses an array list to store game objects
-//10.game object class that is parent to objects created and has tick,render and rectangle(bounding) abstract methods
-//11.key input class
-//12.bit operating system to check for pixel color 
-//13.swap through sprites and animations using array positioning
-//14.use coordinate system for objects and bounding and game state mouse adapter.
-//15.implement all methods and use heirachy
+/* Pseudocode
+ * 1. Extend canvas and implement runnable(thread)
+ * 2. Create our window frame (has width, height, and game)
+ * 3. Intiialize our game 
+ * 4. Create run, tick and render methods
+ * 5. Create init method for intializing stuff
+ * 6. Ability to loop thru and change frames
+ * 7. Use buffer strategy
+ * 8. Use enums for ID AND GAME STATE
+ * 9. Use a handler class that uses an array list to store game objects
+ * 10. Game object class that is parent to objects created and has tick,render and rectangle(bounding) abstract methods
+ * 11. Key input class
+ * 12. Bit operating system to check for pixel color 
+ * 13. Swap through sprites and animations using array positioning
+ * 14. Use coordinate system for objects and bounding and game state mouse adapter.
+ * 15. Implement all methods and use heirachy
+ * */
 
 package com.Bean.Beans.Window;
 
@@ -41,51 +41,48 @@ import com.Bean.Beans.Framework.ObjectId;
 import com.Bean.Beans.Framework.Texture;
 import com.Bean.Beans.Objects.Player;
 
-//game class what runs the game
-public class mainGame extends Canvas implements Runnable { 
-	
-	private static final long serialVersionUID = -571047980453441592L;
+// game class that runs the game
+public class mainGame extends Canvas implements Runnable {
 
-// we extend the  runnable interface
-    
-	//starting our thread
+	private static final long serialVersionUID = -571047980453441592L;
+	// we implement runnable interface to avoid making a thread class a derived class of the class Thread
+
+	// starting our thread
 	private boolean running = false;
 	private Thread thread;
-	
-	static MainMenu menu; //creates class of type menu
-	
-	//tells the current state that the game is in
-	public enum STATE{
-		Menu,
-		Game
+
+	static MainMenu menu; // creates class of type menu
+
+	// tells the current state that the game is in
+	public enum STATE {
+		Menu, Game
 	};
-	
+
 	public STATE gameState = STATE.Menu;
-	
-	public static int width ,height;
-	
-	
+
+	public static int width, height;
+
 	BufferedImage level = null;
-	//handler is better than doing test1,test2,test3 etc
+	// handler is better than doing test1, test2, test3 etc
 	static BufferedImageLoader loader;
 	public static BufferedImage bg = null;
-	
-	//Object
+
+	// Objects
 	Handler handler;
 	Camera cam;
 	static Texture tex;
-	
+
 	public static int LEVEL = 1;
-	
-	private void init() { //initialises everything - gets called before we start our loop
-		
-		
+
+	// initialises everything - gets called before we start our loop
+	private void init() { 
+
 		width = getWidth();
 		height = getHeight();
-		
-	    tex = new Texture();
-	    
-		 loader = new BufferedImageLoader();
+
+		tex = new Texture();
+
+		loader = new BufferedImageLoader();
 		level = loader.loadImage("/Level_1.png");
 		bg = loader.loadImage("/background.png");
 
@@ -95,26 +92,22 @@ public class mainGame extends Canvas implements Runnable {
 		menu = new MainMenu(this, handler);
 		this.addKeyListener(new KeyInput(handler));
 		this.addMouseListener(menu);
-	    if(gameState == STATE.Game)
-		handler.LoadImageLevel(level);
-		
-		//this.addKeyListener(new KeyInput(handler)); //in order for key input to work we must add the new keyinput
+		if (gameState == STATE.Game)
+			handler.LoadImageLevel(level);
 	}
-	
+	// for the purpose of preventing the creating and starting a new thread when one's already started and initialized
 	public synchronized void Start() {
-		if(running) 
+		if (running)
 			return;
 		running = true;
-		
-		thread = new Thread(this); //starts thread
+
+		thread = new Thread(this); // starts thread
 		thread.start();
-		
-		
+
 	}
-	
-	public void run() { //for the thread required by the runnable interface		
-		//OUR GAME LOOP
-		//runs the game at 60 frames per second
+	// for the thread - method required by the runnable interface
+	public void run() { 
+		// OUR GAME LOOP - runs the game at 60 frames per second
 		init();
 		this.requestFocus();
 		long lastTime = System.nanoTime();
@@ -124,21 +117,21 @@ public class mainGame extends Canvas implements Runnable {
 		long timer = System.currentTimeMillis();
 		int updates = 0;
 		int frames = 0;
-		while(running){
+		while (running) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while(delta >= 1){
+			while (delta >= 1) {
 				tick();
 				updates++;
 				delta--;
 			}
-			
+
 			render();
-			
+
 			frames++;
-					
-			if(System.currentTimeMillis() - timer > 1000){
+
+			if (System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
 				System.out.println("FPS: " + frames + " TICKS: " + updates);
 				frames = 0;
@@ -146,67 +139,65 @@ public class mainGame extends Canvas implements Runnable {
 			}
 		}
 	}
- 
+	
+	private void tick() {
 
-private void tick() {
-		
-		if(gameState == STATE.Game) { //if we are the game state
-			handler.tick(); //ticks all the objects
-			for (int i = 0; i<handler.object.size(); i++) {
+		if (gameState == STATE.Game) { // if we are in the game state
+			handler.tick(); // ticks all the objects
+			for (int i = 0; i < handler.object.size(); i++) {
 				if (handler.object.get(i).getId() == ObjectId.Player) {
-					cam.tick(handler.object.get(i)); //object we pass in paramater at index i must be player -- ticks the player with the proper object we want
+					cam.tick(handler.object.get(i)); // object we pass in paramater at index i must be player -- ticks
+														// the player with the proper object we want
 				}
 			}
-		}else if(gameState == STATE.Menu) {
-			
+		} else if (gameState == STATE.Menu) {
 			menu.tick();
 		}
 	}
- private void render() {
-	 // to get our buffering
-	  BufferStrategy bs = this.getBufferStrategy(); // this refers to the canvas class btw
-	  //Initialize once since buffer gets called constantly
-	 
-	  if(bs == null) 
-	  { 
-		  this.createBufferStrategy(3);
-		 // this.isDisplayable();
-		   //triple buffering -- sets amount of buffers behind our first image
-		  return;
-	  }
-	  Graphics g = bs.getDrawGraphics(); // gets graphics context for our buffering
-	  //cast to 2d for camera
-	  Graphics2D g2D = (Graphics2D) g; //has translate function
-	 
-	  ////drawing our graphics//////
-	  g.fillRect(0, 0,  getWidth(), getHeight());// our surface
-	  
-	  g.drawImage(bg, 0, 0, null);
 	
-	  g2D.translate(cam.getX(), cam.getY()); //translate everything it sandwiches  //begin of cam
-	  
-	  //anything in btwn g2d start of cam and end of cam is going to be affected by the camera which is  -- handler.render
-	  
-	  handler.render(g);
-	  if(gameState == STATE.Game) {
-		  g2D.translate(-cam.getX(), -cam.getY());  //end of cam
-	  }else if(gameState == STATE.Menu) {
-			menu.render(g);//renders Main Menu
-	  }else {
-		  g.setColor(Color.white);
-		  g.drawString("Menu", 100, 100);
-	  }
-	  
-		  /////////////////////
-	 g.dispose(); // disposes of this graphics context
-	 bs.show(); // . show makes next bufferstart visible
+	private void render() {
+		// to get our buffering
+		BufferStrategy bs = this.getBufferStrategy(); // this refers to the canvas class btw
+		// Initializes once since buffer gets called constantly
+
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			// triple buffering -- sets amount of buffers behind our first image
+			return;
+		}
+		Graphics g = bs.getDrawGraphics(); // gets graphics context for our buffering
+		// cast to 2d for camera
+		Graphics2D g2D = (Graphics2D) g; // has translate function
+
+		//// drawing our graphics ////
+		g.fillRect(0, 0, getWidth(), getHeight()); // our surface
+
+		g.drawImage(bg, 0, 0, null);
+
+		g2D.translate(cam.getX(), cam.getY()); // translate everything it sandwiches // begin of cam
+
+		// anything in btwn g2d start of cam and end of cam is going to be affected by
+		// the camera which is -- handler.render
+
+		handler.render(g);
+		if (gameState == STATE.Game) {
+			g2D.translate(-cam.getX(), -cam.getY()); // end of cam
+		} else if (gameState == STATE.Menu) {
+			menu.render(g);// renders Main Menu
+		} else {
+			g.setColor(Color.white);
+			g.drawString("Menu", 100, 100);
+		}
+
+		g.dispose(); // disposes of this graphics context
+		bs.show(); // show makes next bufferstart visible
 	}
 
- 	public static Texture getInstance() {  //gets instance of our texture  -- in player class just say maingame.getisnatnce()
-	 	return tex;
- 	}
- 	
- 	public static void main(String[] args) {
-  		new beanWindow(800, 600 , "Happy Bean runner", new mainGame()); //creates a window for us 
+	public static Texture getInstance() { // gets instance of our texture -- in player class just say
+		return tex;
+	}
+
+	public static void main(String[] args) {
+		new beanWindow(800, 600, "Happy Bean runner", new mainGame()); // creates our window
 	}
 }
